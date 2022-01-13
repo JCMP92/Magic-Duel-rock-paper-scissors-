af852e23219fdcc902d221251638de73e1fbf7d3
JCMP92 <arq_juanmp@outlook.com>
2022-01-13 17:10:31 -0600

    Added buttons, just working with console.log

--- script.js
+++ script.js
@@ -3,50 +3,60 @@ function game() {
     let defeat = 0;
     const round = 5;
 
-    for (let i = 0; i < round; i++) {
-    let playerSelection = prompt("Choose one: Rock, Paper or Scissors").toLowerCase();
-    function computerPlay() {
-        const spells = ["rock", "paper", "scissors"];
-        return spells[Math.floor(Math.random() * spells.length)];
-        };
-    let computerSelection = computerPlay();
-        let gameRound = playRound(playerSelection, computerSelection);
-    if(gameRound === "Draw"){
-    console.log("Meh... It´s a Draw.");
-    }else if(gameRound === "Loose"){
-    defeat ++;
-    console.log("You loose this one.");
-    }else if(gameRound === "Win"){
-        victory ++;
-        console.log("Great! you are awesome.");
-      }
-    };
-     
-    let score ={
-        'Wins':victory,
-        'Defeats':defeat,
-    }; 
-    if (victory > defeat) {
-        console.log("Amazing! you are the new Champion")
-        return score;
-    } else {
-        console.log("You died...")
-        return score;
-    };
+    document.getElementById("rock").addEventListener('click', function () {
+        console.log('rock');
+    });
+    document.getElementById("paper").addEventListener('click', function () {
+        console.log('paper');
+    });
+    document.getElementById("scissors").addEventListener('click', function () {
+        console.log('scissors');
+    });
 
-    function playRound(playerSelection, computerSelection) {
-    
-            if(playerSelection === computerSelection){
-                return "Draw";
-            } else if (playerSelection === "rock" && computerSelection === "paper" 
-            || playerSelection === "scissors" && computerSelection === "rock" 
-            || playerSelection === "paper" && computerSelection === "scissors"){
-                return "Loose";
+    // for (let i = 0; i < round; i++) {
+    // let playerSelection = prompt("Choose one: Rock, Paper or Scissors").toLowerCase();
+    // function computerPlay() {
+    //     const spells = ["rock", "paper", "scissors"];
+    //     return spells[Math.floor(Math.random() * spells.length)];
+    //     };
+    // let computerSelection = computerPlay();
+    //     let gameRound = playRound(playerSelection, computerSelection);
+    // if(gameRound === "Draw"){
+    // console.log("Meh... It´s a Draw.");
+    // }else if(gameRound === "Loose"){
+    // defeat ++;
+    // console.log("You loose this one.");
+    // }else if(gameRound === "Win"){
+    //     victory ++;
+    //     console.log("Great! you are awesome.");
+    //   }
+    // };
      
-            } else { 
-                return "Win";
+    // let score ={
+    //     'Wins':victory,
+    //     'Defeats':defeat,
+    // }; 
+    // if (victory > defeat) {
+    //     console.log("Amazing! you are the new Champion")
+    //     return score;
+    // } else {
+    //     console.log("You died...")
+    //     return score;
+    // };
+
+    // function playRound(playerSelection, computerSelection) {
     
-            }        
-    } 
+    //         if(playerSelection === computerSelection){
+    //             return "Draw";
+    //         } else if (playerSelection === "rock" && computerSelection === "paper" 
+    //         || playerSelection === "scissors" && computerSelection === "rock" 
+    //         || playerSelection === "paper" && computerSelection === "scissors"){
+    //             return "Loose";
+     
+    //         } else { 
+    //             return "Win";
+    
+    //         }        
+    // } 
 };
 
